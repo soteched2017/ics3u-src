b1e10c23e7653da395890bcba834eb382f1b97eb
virtual mr. so <soteched2017@gmail.com>
2020-09-16 08:14:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+/**
+* Examples for Computer Science, 11 
+* Toronto, Canada
+*
+* https://github.com/soteched2017/ics3u-src
+* https://repl.it/@soteched2017/ics3u-src#Main.java
+*
+* modified 20200916
+* date     20200705
+* @author  unknown and vmso
+* @version 1.0
+* @see     units 1 through 4
+*/
+
+class Main {
+
+    public static void main (String[] args) {
+
+        new unit1.Unit1Main().main(null);
+        
+    }
+
+}
